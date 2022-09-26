bd0f20f490ca3e9c7e66b7e4b83e34142b84ebde
mattneary1 <mattneary1@gmail.com>
2022-09-26 16:35:36 -0700

    Added budget card component and initial styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Stack, Button } from "react-bootstrap";
+import BudgetCard from "./components/BudgetCard";
 
 function App() {
   return (
-    null
+    <Container className="my-4">
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+          gap: "1rem",
+          alignItems: "flex-start",
+        }}
+      >
+        <BudgetCard name="Entertainment" amount={200} max={1000}></BudgetCard>
+      </div>
+    </Container>
   );
 }
 
